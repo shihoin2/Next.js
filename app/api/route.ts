7f1fefa252e59c88cb435bbdbcaf0a3b73fc0544
shihoin2 <merysousi@gmail.comm>
2024-01-20 15:14:58 +0900

    天気情報表示

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,8 +4,7 @@ import { InferGetStaticPropsType, GetServerSideProps } from "next";
 
 export async function GET(req: NextRequest) {
   const res = await
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=34&lon=135&appid=d068bb96a1d7ba8987f892ea7c2d81ab`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=34&lon=135&units=metric&appid=d068bb96a1d7ba8987f892ea7c2d81ab`)
   const data = await res.json()
   return NextResponse.json({data});
-
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,41 @@ import Link from 'next/link';
 import { use } from 'react';
 
 
+"use client"
+import React, { useState, useEffect } from 'react';
 
-export default function Page() {
+
+const Page: React.FC = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api');
+        const data = await res.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-    <div>
-      <h1>お住まいの地域の天気</h1>
-      <p>最高気温：12℃</p>
-    </div>
-  </>
+      <div>
+        <h1>お住まいの地域の天気</h1>
+        {weatherData && (
+          <>
+            <p> {weatherData.data.weather[0].icon} </p>
+            <p> {weatherData.data.weather[0].main} </p>
+            <p>最高気温: {weatherData.data.main.temp_max} ℃</p>
+          </>
+        )}
+      </div >
+    </>
   );
-  }
+};
+
+export default Page;
